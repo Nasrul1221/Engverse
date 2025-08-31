119e113d92d1f7a12a95884eb091202aeb094831
nasrul1221 <fst20071221@gmail.com>
2025-08-31 11:24:32 +0300

    Updates

--- assets/react-0CjUggY7.js
+++ assets/react-0CjUggY7.js
@@ -1,4 +1,4 @@
-import{R as I}from"./index-CDSw6D-e.js";import{C as x}from"./custom-media-element-Ce0vsE-t.js";import{M as A,H as m}from"./hls-DFLUVhEf.js";const H=e=>class extends e{static shadowRootOptions={...e.shadowRootOptions};static getTemplateHTML=(t,n={})=>{const{src:h,...E}=t;return`
+import{R as I}from"./index-C726z2VJ.js";import{C as x}from"./custom-media-element-Ce0vsE-t.js";import{M as A,H as m}from"./hls-DFLUVhEf.js";const H=e=>class extends e{static shadowRootOptions={...e.shadowRootOptions};static getTemplateHTML=(t,n={})=>{const{src:h,...E}=t;return`
         <script type="application/json" id="config">
           ${JSON.stringify(n.config||{})}
         <\/script>
