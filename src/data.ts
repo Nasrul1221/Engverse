cf03284c59c4440381a9591575db00ef85ffaa4b
nasrul1221 <fst20071221@gmail.com>
2025-08-31 13:47:41 +0300

    fix: correct grammatical errors in lesson content

--- src/data.ts
+++ src/data.ts
@@ -7,10 +7,10 @@ export const lessonData: LessonsData = {
         title: "Present Simple 'to be'",
         id: 1,
         content:
-          "## Present simple forms of '**to be**'\n The verb **'to be'** is one of the most important verbs in English. It is used to describe people, things, feelings, professions, nationality, location, and states. \n| Subject | Form | Positive | Negative | Question |\n|---------|------|---------|----------|----------|\n| I | am | I am | I am not | Am I |\n| You | are | You are | You are not | Are you |\n| He\\She\\It | is | She is | He is not | Is it |\n| We | are | We are | We are not | Are we |\n| They | are | They are | They are not | Are they |\n\n As you may notice we this structure - ***Subject + verb***\n\n ***Subject*** is someone or something that completes an action.",
+          "## Present simple forms of '**to be**'\n The verb **'to be'** is one of the most important verbs in English. It is used to describe people, things, feelings, professions, nationality, location, and states. \n| Subject | Form | Positive | Negative | Question |\n|---------|------|---------|----------|----------|\n| I | am | I am | I am not | Am I |\n| You | are | You are | You are not | Are you |\n| He\\She\\It | is | She is | He is not | Is it |\n| We | are | We are | We are not | Are we |\n| They | are | They are | They are not | Are they |\n\n As you may notice we use this structure - ***Subject + verb***\n\n ***Subject*** is someone or something that completes an action.",
         examples: [
           '### Examples',
-          '\n #### **Subject - I**\n - I am doctor\n - I am hungry\n - I am a teacher',
+          '\n #### **Subject - I**\n - I am a doctor\n - I am hungry\n - I am a teacher',
           '\n***\n #### **Subject - You**\n - You are beautiful\n - You are too young\n - You are the best',
           "\n***\n #### **Subject - He\\She\\It**\n - He is my friend\n - She is my brother's wife\n - It is my book",
           '\n***\n#### **Subject - We**\n- We are students\n- We are from Ukraine\n- We are happy\n',
