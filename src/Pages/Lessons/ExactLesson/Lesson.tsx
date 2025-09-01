01366e0c1f6ebfc99a25387a64e824f80dbe43d4
nasrul1221 <fst20071221@gmail.com>
2025-09-01 16:43:58 +0300

    feat: replace Link with NavLink for improved active state handling in lesson navigation

--- src/Pages/Lessons/ExactLesson/Lesson.tsx
+++ src/Pages/Lessons/ExactLesson/Lesson.tsx
@@ -1,26 +1,27 @@
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import '../styles.css';
-import { useState } from 'react';
 
 export default function Lesson() {
-  const [active, setActive] = useState<'lesson' | 'exercises'>('lesson');
   return (
     <div>
       <div className="d-flex align-items-center justify-content-center mt-3 gap-3">
-        <Link
+        <NavLink
           to="."
-          className={`btn ${active === 'lesson' ? 'btn-primary' : 'btn-outline-primary'}`}
-          onClick={() => setActive('lesson')}
+          end
+          className={({ isActive }) =>
+            isActive ? 'btn btn-primary' : 'btn btn-outline-primary'
+          }
         >
           Lesson
-        </Link>
-        <Link
-          className={`btn ${active === 'exercises' ? 'btn-primary' : 'btn-outline-primary'}`}
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? 'btn btn-primary' : 'btn btn-outline-primary'
+          }
           to="exercises"
-          onClick={() => setActive('exercises')}
         >
           Exercises
-        </Link>
+        </NavLink>
       </div>
       <div>
         <Outlet />
